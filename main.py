667bdc878b450e9f24ada508d46d0bab808baba1
eldobbo93 <liamdobson6493@googlemail.com>
2025-07-24 13:51:14 +0100

    Generating the report

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
-from stats import count_words, count_each_char
+from stats import count_words, count_each_char, sort_dict, sort_on
 
 def main():
-    text = get_book_text("./books/frankenstein.txt")
+    print("============ BOOKBOT ============")
+    book="./books/frankenstein.txt"
+    text = get_book_text(book)
+    print("Analyzing book found at " + book)
+    
+    print("----------- Word Count ----------")
     word_count = count_words(text)
+    print(f"Found {word_count} total words")
+    
+    print("--------- Character Count -------")
     char_count = count_each_char(text)
-    print(f"{word_count} words found in the document")
-    print(char_count) 
+    sorted_char_count = sort_dict(char_count) 
+    for i in sorted_char_count:
+        if i["char"].isalpha():
+            print(f"{i["char"]}: {i["num"]}")
+    print("============= END ===============")
 
 def get_book_text(path):
     with open(path) as path:
--- stats.py
+++ stats.py
@@ -9,3 +9,16 @@ def count_each_char(text):
         else:
             res[char] = 1
     return res
+
+def sort_on(items):
+    return items["num"]
+
+def sort_dict(dict):
+    list=[]
+    for i in dict:
+        char_dict = {}
+        char_dict["char"] = i
+        char_dict["num"] = dict[i]
+        list.append(char_dict)
+    list.sort(reverse=True, key=sort_on)
+    return list
