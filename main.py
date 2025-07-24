9a64f91d6527f5bc48f1c055aaa4c0d12ae1f031
eldobbo93 <liamdobson6493@googlemail.com>
2025-07-24 14:02:05 +0100

    Adding CLI args

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 from stats import count_words, count_each_char, sort_dict, sort_on
+import sys
 
 def main():
+    #Check correct number of args given
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
     print("============ BOOKBOT ============")
-    book="./books/frankenstein.txt"
+    book=sys.argv[1]
     text = get_book_text(book)
     print("Analyzing book found at " + book)
     
